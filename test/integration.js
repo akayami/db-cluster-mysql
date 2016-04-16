a70ac5fa17b7a8fcccdb634648c4d5f1a8def947
Tomasz Rakowski <tomasz@akayami.com>
2016-04-16 07:56:41 -0400

    Fixed test and upped version

--- test/integration.js
+++ test/integration.js
@@ -26,11 +26,11 @@ var config = {
 						var pool = this.pool;
 						pool.getConnection(function(err, conn) {
 							conn.query('select (FLOOR(1 + RAND() * 100)) as number', function(err, res) {
-								conn.release();
-								if (res[0].number % 2 == 0) {
-									poolObject.paused = true;
+								conn.release();								
+								 if (res.rows()[0].number % 2 == 0) {
+								 	poolObject.paused = true;
 								} else {
-									poolObject.paused = false;
+								 	poolObject.paused = false;
 								}
 							});
 						})
